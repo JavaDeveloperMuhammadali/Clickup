bf2fb58c21a3cab42030321825ca48c75642cc90
Muhammadali Oltinov <muhammadalioltinov@gmail.com>
2025-04-02 16:16:28 +0500

    add

--- src/main/java/org/example/clickup/model/Users.java
+++ src/main/java/org/example/clickup/model/Users.java
@@ -1,7 +1,31 @@
 package org.example.clickup.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String name;
-    private String asfasdf;
+    @Column(nullable = false)
+    private String full_name;
+    @Column(nullable = false ,unique = true)
+    private String email;
+    @Column(nullable = false , unique = true)
+    private String password;
+    @Column(nullable = false)
+    private String collor;
+    @Column(nullable = false)
+    private String inital_letter;
+    @ManyToMany
+    List<attachmet>attachmet;
+
 }
---
+++ src/main/java/org/example/clickup/model/workspace.java
@@ -0,0 +1,27 @@
+package org.example.clickup.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class workspace {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String color;
+    @OneToOne
+    private Users owner_id;
+    @Column(nullable = false)
+    private String initial_letter;
+    @Column(nullable = false)
+    private String avatar_Id;
+
+}
